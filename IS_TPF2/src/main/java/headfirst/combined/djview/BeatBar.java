3c96e1b9c6f8bc26b2ad948533a6d9850f269360
AgileG <slgaby92@gmail.com>
2015-06-24 14:20:33 -0300

    modificaaciones testing!

--- IS_TPF2/src/main/java/headfirst/combined/djview/BeatBar.java
+++ IS_TPF2/src/main/java/headfirst/combined/djview/BeatBar.java
@@ -5,12 +5,25 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class BeatBar extends JProgressBar implements Runnable { 
-/* 	codigo complejo
-	ok
-	ok
-	ok
-	ok
-	fin
-*/
+	JProgressBar progressBar;
+	Thread thread;
+
+	public BeatBar() {
+		thread = new Thread(this);
+		setMaximum(100);
+		thread.start();
+	}
+
+	public void run() {
+		for(;;) {
+			int value = getValue();
+			value = (int)(value * .75);
+			setValue(value);
+			repaint();
+			try {
+				Thread.sleep(50);
+			} catch (Exception e) {};
+		}
+	}
 
 }
